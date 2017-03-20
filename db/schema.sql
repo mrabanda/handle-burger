d5e494213b86d8731587d586a6e8d791aa1e237a
mrabanda <mrabanda@gmail.com>
2017-03-20 01:05:27 -0400

    fixed typo in JAWSBD connection

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(255) NOT NULL,
   devoured  BOOLEAN,
-  date TIMESTAMP,
+  date TIMESTAMP NOT NULL,
   PRIMARY KEY (id)
 );
